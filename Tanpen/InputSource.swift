ea26bd96fc1449829c45bff7d2bd91211bb428a6
Toto Minai <the@unpopular.me>
2022-12-28 12:06:49 +0900

    Add InputSource.swift

---
+++ Tanpen/InputSource.swift
@@ -0,0 +1,30 @@
+//
+//  InputSource.swift
+//  Tanpen
+//
+//  Created by Toto Minai on 27/12/2022.
+//
+
+import InputMethodKit
+
+class InputSource {
+    static var sources: [TISInputSource] {
+        TISCreateInputSourceList(nil, false).takeRetainedValue() as! [TISInputSource]
+    }
+
+    static func select(with id: String) {
+        TISSelectInputSource(sources.first { $0.id == id })
+    }
+}
+
+extension TISInputSource {
+    private func getProperty(_ key: CFString) -> AnyObject {
+        return Unmanaged<AnyObject>
+            .fromOpaque(TISGetInputSourceProperty(self, key))
+            .takeUnretainedValue()
+    }
+
+    var id: String { getProperty(kTISPropertyInputSourceID) as! String }
+    var localizedName: String { getProperty(kTISPropertyLocalizedName) as! String }
+    var isSelectCapable: Bool { getProperty(kTISPropertyInputSourceIsSelectCapable) as! Bool }
+}
