f4958b251c0d4e896ee58ace1b43681ac64fc4cc
Toto Minai <the@unpopular.me>
2023-01-07 00:35:12 +0900

    Change Settings Window configuration

--- Tanpen/Management/WindowDelegate.swift
+++ Tanpen/Management/WindowDelegate.swift
@@ -19,7 +19,7 @@ class WindowDelegate: NSObject, NSWindowDelegate {
         window.backgroundColor = NSColor(white: 1, alpha: 1e-3)
         
         window.tabbingMode = .disallowed
-        window.delegate = self
+//        window.delegate = self
     }
     
     override init() {
@@ -27,8 +27,13 @@ class WindowDelegate: NSObject, NSWindowDelegate {
         
         NotificationCenter.default.addObserver(forName: NSWindow.didChangeOcclusionStateNotification,
                                                object: nil, queue: nil) { notification in
-            if let window = notification.object as? NSWindow {
-                self.configure(window: window)
+            if let window = notification.object as? NSWindow,
+               window.className != "NSMenuWindowManagerWindow" {
+                if (window.frameAutosaveName == "com_apple_SwiftUI_Settings_window") {
+                    window.standardWindowButton(.zoomButton)?.isHidden = true
+                    window.standardWindowButton(.miniaturizeButton)?.isHidden = true
+                    window.backgroundColor = NSColor.windowBackgroundColor
+                } else { self.configure(window: window) }
             }
         }
     }
